79cddd77518281077f857c174b2328b67b57ab09
VamsiKalyan SridharBabu <vamsisridhar@hotmail.com>
2021-03-14 22:20:23 +0000

    update loop complete

--- grid.py
+++ grid.py
@@ -1,24 +1,64 @@
-import pygame
+import pygame, sys
 import numpy as np
 class Grid:
+
+    shapes = {
+        "block" : [(0,0), (0,1), (1,0), (1,1)]
+    }
+
     def __init__(self, size: tuple, surface: pygame.Surface, p_size: int):
         self.resolution = tuple([i // p_size for i in size])
-        grids = [np.empty(self.resolution)]*2
-        self.p_grid = grids[0] #Primary
-        self.p_grid.fill(0)
-        self.s_grid = grids[1] #Secondary
+        self.p_grid = np.zeros(self.resolution) #Primary
+        self.s_grid = np.zeros(self.resolution)#Secondary
         self.surface = surface
         self.p_size = p_size
-        
 
 
     def draw_grid(self):
         for i in range(self.resolution[0]):
             for j in range(self.resolution[1]):
                 color = (0,0,0)
-                if self.p_grid[i][j] == 1:
+                if self.p_grid[j][i] == 1:
                     color = (255, 255, 255)
                 pygame.draw.rect(self.surface, color, tuple([i * self.p_size for i in [i, j, 1, 1]]))
     
-    def set_pixel(self, g_pos:tuple, val:int):
-        self.p_grid[g_pos] = val
+    def set_pixel(self, x, y, val:int):
+        self.p_grid[y, x] = val
+
+    def draw_object(self, name, x, y):
+        for pt in self.shapes[name]:
+            if pt[0] + x < self.resolution[0] and pt[1] + y < self.resolution[1]:
+                self.set_pixel(pt[0] + x, pt[1] + y, 1)
+                
+                
+
+    def update(self):
+        for i in range(self.resolution[0]):
+            for j in range(self.resolution[1]):
+                n_count = 0
+                
+                for x in range(i-1, i+2):
+                    for y in range(j-1, j+2):
+
+                        if x > 0 and y > 0 and x < self.resolution[0] and y < self.resolution[1]:
+                            
+                            if (x, y) != (i,j):
+                                if self.p_grid[y][x]:
+                                    n_count += 1
+
+                if self.p_grid[j][i] == 0:
+                    if n_count == 3:
+                        self.s_grid[j][i] = 1
+                    else:
+                        self.s_grid[j][i] = 0
+
+                elif self.p_grid[j][i] == 1:
+                    
+                    if n_count < 2 or n_count > 3:
+                        self.s_grid[j][i] = 0
+                    else:
+                        self.s_grid[j][i] = 1
+        
+        self.p_grid = np.copy(self.s_grid)
+        self.s_grid.fill(0)
+        
--- main.py
+++ main.py
@@ -11,14 +11,39 @@ screen = pygame.display.set_mode(size)
 p_size = 5
 
 m_grid = Grid(size, screen, p_size)
+mouse_b = 0
+anim =False
+fps = 60
+
+m_grid.draw_object("block",35, 35)
+
 
 while True:
+    time.sleep(1/fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            m_pos = pygame.mouse.get_pos()
-            m_grid.set_pixel(tuple([i // p_size for i in m_pos]), 1)
-    
+        if anim == False:
+            fps = 60
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0]:
+                    mouse_b = 2
+                elif pygame.mouse.get_pressed()[2]:
+                    mouse_b = 1
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_b = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                anim = not anim
+    if mouse_b != 0:
+        m_pos = pygame.mouse.get_pos()
+        g_pos = tuple([i // p_size for i in m_pos])
+        print(g_pos)
+        m_grid.set_pixel(g_pos[0], g_pos[1], mouse_b - 1)
+
+    if anim:
+        fps = 2
+        m_grid.update()
+
     m_grid.draw_grid()
 
     pygame.display.flip()
